1c950d2cd0ab1c762f372f6fdb36a51bc5d218f7
Evandro Barbosa dos Reis <evandrobarbosadosreis@gmail.com>
2021-01-28 14:39:16 -0300

    commit inicial do projeto

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.Azure.Management.Compute.Fluent.Models;
+using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+
+namespace az_204_vm_deploy
+{
+    class Program
+    {
+
+        static string ReaderAdminUser()
+        {
+            Console.Write("Digite o nome do usuário administrador: ");
+            return Console.ReadLine();
+        }
+
+        static string ReadAdminPassword()
+        {
+            Console.Write("Digite a senha: ");
+            return Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            //autenticação da aplicação
+            var credentials = SdkContext.AzureCredentialsFactory.FromFile("./azureauth.properties");
+
+            var azure = Azure.Configure()
+                .WithLogLevel(HttpLoggingDelegatingHandler.Level.Basic)
+                .Authenticate(credentials)
+                .WithDefaultSubscription();
+
+            //criando um resource group
+            var groupName = "az-204-resource-group";
+            var region    = Region.USEast2;
+
+            Console.WriteLine($"Resource Group: {groupName}");
+            var resourceGroup = azure.ResourceGroups.Define(groupName)
+                .WithRegion(region)
+                .Create();
+
+            //criando uma virtual network e uma virtual subnet
+            var networkName   = "az-204-virtual-network";
+            var networkAdress = "192.0.0.0/16";
+            var subnetName    = "az-204-virtual-subnet";
+            var subnetAdress  = "192.0.0.0/24";
+
+            Console.WriteLine($"Virtual Network: {networkName}");
+            var network = azure.Networks.Define(networkName)
+                .WithRegion(region)
+                .WithExistingResourceGroup(resourceGroup)
+                .WithAddressSpace(networkAdress)
+                .WithSubnet(subnetName, subnetAdress)
+                .Create();
+
+            //cria um network interface card para conectar a vm na network
+            var networkInterfaceName = "az-204-network-interface-card";
+            
+            Console.WriteLine($"Virtual Network Interface Card: {networkInterfaceName}");
+            var networkInterface = azure.NetworkInterfaces.Define(networkInterfaceName)
+                .WithRegion(region)
+                .WithExistingResourceGroup(resourceGroup)
+                .WithExistingPrimaryNetwork(network)
+                .WithSubnet(subnetName)
+                .WithPrimaryPrivateIPAddressDynamic()
+                .Create();
+
+            //cria a máquina virtual
+            var virtualMachineName = "az-204-vm";
+            var adminUser     = ReaderAdminUser();
+            var adminPassword = ReadAdminPassword();
+
+            Console.WriteLine($"Virtual Machine: {virtualMachineName}");
+            var virtualMachine = azure.VirtualMachines.Define(virtualMachineName)
+                .WithRegion(region)
+                .WithExistingResourceGroup(resourceGroup)
+                .WithExistingPrimaryNetworkInterface(networkInterface)
+                .WithLatestWindowsImage("MicrosoftWindowsServer", "WindowsServer", "2019-Datacenter")
+                .WithAdminUsername(adminUser)
+                .WithAdminPassword(adminPassword)
+                .WithComputerName(virtualMachineName)
+                .WithSize(VirtualMachineSizeTypes.StandardDS2V2)
+                .Create();
+
+            Console.WriteLine("Processo concluído");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# az-204-vm-deploy
-Deploy de uma VM em ambiente cloud através do SDK da plataforma Azure
+# AZ-204 Virtual Machine Deploy
+
+Exercício realizado como preparação para o exame AZ-204 demonstrando o processo de configuração e deploy de uma VM por meio do SDK da plataforma Azure.
+
+Através deste exemplo, é possível compreender melhor a relação e quais são os recursos necessários para realizar um deploy de VM em ambiente de núvem.
+
+Os recursos criados neste exemplo são:
+
+- Resource Group;
+- Virtual Network
+- Virtual Network Subnet
+- Virtual Network Interface Card
+- Virtual Machine
+
+Além destes recursos, será criado automaticamente um Virtual Disk. 
+
+## Instalação do SDK
+
+Comando de instalação do pacote: `dotnet add package Microsoft.Azure.Management.Fluent`
+
+## Listagem dos Sistemas Operacionais do Azure Marketplace
+
+Para definir o Sistema Operacional da VM são informados 3 parâmetros:
+
+- Publisher
+- Offer
+- SKU
+
+É possível obter essas informações utilizando o Azure CLI:
+
+`az login`
+
+`az vm image list`
+
+## Configurando o aplicativo
+
+Para que o aplicativo se conecte corretamente em sua conta algumas informações são necessárias no arquivo `azureauth.properties`, sendo:
+
+- subscription-id: Pode ser recuperado no menu de "Subscriptions"
+- tenant-id: Obtido no menu "Azure Active Directory" > "Properties"
+- cliend-id: O client-id é gerado após registrar seu aplicativo na opção "Azure Active Directory" -> "App registrations"
+- client-secret: Após liberar o aplicativo e obter o client-id, gere uma chave de acesso no menu "Certificates & secrets"
+
+Após isso, crie uma "Role Assignment" como "Contribuitor" para o seu aplicativo no menu "Access Control" da sua Subscription.
